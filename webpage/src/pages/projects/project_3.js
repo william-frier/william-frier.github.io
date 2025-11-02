0fc256ca30c54984e8bb06018d999a0813c21dad
William Frier <williamfrier@williams-mbp.home>
2025-11-02 22:17:30 +0100

    haptics pattern on my phone

--- webpage/src/pages/projects/project_3.js
+++ webpage/src/pages/projects/project_3.js
@@ -2,18 +2,22 @@ import React from "react";
 import '../../style.css';
 
 import {
-    Button
+    Button,
+    Row,
+    Col,
+    Container
   } from 'reactstrap';
 
 
 
-function Project3() {
+  function Project3() {
 
-    const handleVibrate = () => {
+    function handleVibrate(time) {
         if ("vibrate" in navigator) {
-          navigator.vibrate(200);
+            navigator.vibrate(0);
+            navigator.vibrate(time);
         } else {
-          alert("Vibration API not supported on this device.");
+            alert("Vibration API not supported on this device.");
         }
       };
     
@@ -22,8 +26,46 @@ function Project3() {
         <div>
             <h1>Project 3 - Haptics</h1>
 
-            <h5><em>Test with Android</em></h5>
-            <Button onClick={handleVibrate}>200ms</Button>
+            <h5><em>Test with Android, no "silent mode"</em></h5>
+
+            <p>My pixel 7 takes shorter than 1s depends on the level of "Vibration and haptics/Interactive Haptics/Touch feeeback" options</p>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <Button onClick={() => handleVibrate([2000])}>2000ms</Button>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => handleVibrate([1500])}>1500ms</Button>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => handleVibrate([1001])}>1001ms</Button>
+                    </Col>
+                </Row>
+                <Row>
+                <Col>
+                        <Button onClick={() => handleVibrate([200])}>200ms</Button>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => handleVibrate([100])}>100ms</Button>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => handleVibrate([50])}>50ms</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button onClick={() => handleVibrate([100, 10, 100])}>100, 10, 100</Button>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => handleVibrate([10, 1, 10])}>10, 1, 10</Button>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => handleVibrate([100, 10, 100])}>50, 20,  100</Button>
+                    </Col>
+                </Row>
+
+            </Container>
 
 
         </div>
